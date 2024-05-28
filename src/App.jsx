2a617d6ba7ad12931b5a5be9843aa2b896d7723a
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2024-05-28 16:52:41 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+import "@tensorflow/tfjs-backend-webgl";
 import Loader from "./components/loader";
 import ButtonHandler from "./components/btn-handler";
 import { detect, detectVideo } from "./utils/detect";
 import "./style/App.css";
-import DeveloperInfo from "./components/developerinfo";
+import Header from "./components/header";
+import DeveloperInfo from "./components/developerinfo"
 
 const App = () => {
-  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [loading, setLoading] = useState({ loading: true, progress: 0 });
   const [model, setModel] = useState({
     net: null,
     inputShape: [1, 0, 0, 3],
-  }); // init model & input shape
+  });
 
-  // references
   const imageRef = useRef(null);
   const cameraRef = useRef(null);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // model configs
   const modelName = "yolov8n";
 
   useEffect(() => {
@@ -29,12 +28,11 @@ const App = () => {
         `${window.location.href}/${modelName}_web_model/model.json`,
         {
           onProgress: (fractions) => {
-            setLoading({ loading: true, progress: fractions }); // set loading fractions
+            setLoading({ loading: true, progress: fractions });
           },
         }
-      ); // load model
+      );
 
-      // warming up model
       const dummyInput = tf.ones(yolov8.inputs[0].shape);
       const warmupResults = yolov8.execute(dummyInput);
 
@@ -42,49 +40,60 @@ const App = () => {
       setModel({
         net: yolov8,
         inputShape: yolov8.inputs[0].shape,
-      }); // set model & input shape
+      });
 
-      tf.dispose([warmupResults, dummyInput]); // cleanup memory
+      tf.dispose([warmupResults, dummyInput]);
     });
   }, []);
 
   return (
     <div className="App">
-      {loading.loading && <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>}
-      <div className="header">
-        <h1>📷 TSR Recognition App</h1>
-        <p>
+      <Header />
+
+      <div className="header-info">
+      <h2 class='spaceid'>
           YOLOv8 live detection application on browser powered by <code>tensorflow.js</code>
-        </p>
-        <p>
+        </h2>
+        <h2 class='spaceid'>
           Serving : <code className="code">{modelName}</code>
-        </p>
+        </h2>
       </div>
 
-      <div className="content">
-        <img
-          src="#"
-          ref={imageRef}
-          onLoad={() => detect(imageRef.current, model, canvasRef.current)}
-        />
-        <video
-          autoPlay
-          muted
-          ref={cameraRef}
-          onPlay={() => detectVideo(cameraRef.current, model, canvasRef.current)}
-        />
-        <video
-          autoPlay
-          muted
-          ref={videoRef}
-          onPlay={() => detectVideo(videoRef.current, model, canvasRef.current)}
-        />
-        <canvas width={model.inputShape[1]} height={model.inputShape[2]} ref={canvasRef} />
+      <main className="main-content">
+        {loading.loading && (
+          <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>
+        )}
+        <div className="content">
+          <img
+            src="#"
+            ref={imageRef}
+            onLoad={() => detect(imageRef.current, model, canvasRef.current)}
+          />
+          <video
+            autoPlay
+            muted
+            ref={cameraRef}
+            onPlay={() => detectVideo(cameraRef.current, model, canvasRef.current)}
+          />
+          <video
+            autoPlay
+            muted
+            ref={videoRef}
+            onPlay={() => detectVideo(videoRef.current, model, canvasRef.current)}
+          />
+          <canvas width={model.inputShape[1]} height={model.inputShape[2]} ref={canvasRef} />
+        </div>
+
+        <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} videoRef={videoRef} />
+      </main>
+
+      <div>
+         <DeveloperInfo />
       </div>
 
-      <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} videoRef={videoRef} />
-
-      <DeveloperInfo />
+      <footer className="footer">
+        <p>&copy; 2024 TSR Recognition App. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
